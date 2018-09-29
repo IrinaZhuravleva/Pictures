f8b97d0a8ff35de83eaba5cce9a8e09617a8e299
Irina Zhuravleva <irinazhuravleva@MacBook-Pro-Irina.local>
2018-09-29 19:31:20 +0300

    Добавление меню по клику

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//   var link = $('.main-nav__toggle');
+//   var link_active = $('.blockmenu_active');
+//   var menu = $('.blockmenu');
+//   var close = $('.blockmenu-close');
+
+
+//   link.click(function(){
+//     menu.addClass('blockmenu_active');
+//     link.css('display', 'none');
+//   });
+//   close.click(function(){
+//     menu.removeClass('blockmenu_active');
+//   });
+//   close.click(function(){
+//     link.css('display', 'block');
+//   });
+
+// });
