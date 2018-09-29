903444c35e8a50d357a4e21144ea09b27f18acca
Irina Zhuravleva <irinazhuravleva@MacBook-Pro-Irina.local>
2018-09-29 20:27:37 +0300

    File jquery

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-// $(document).ready(function(){
-//   var link = $('.main-nav__toggle');
-//   var link_active = $('.blockmenu_active');
-//   var menu = $('.blockmenu');
-//   var close = $('.blockmenu-close');
+$(document).ready(function(){
+  var link = $('.navbar-toggler');
+  // var link = $('.main-nav__toggle');
+  var link_active = $('.blockmenu_active');
+  var menu = $('.blockmenu');
+  var close = $('.blockmenu-close');
 
 
-//   link.click(function(){
-//     menu.addClass('blockmenu_active');
-//     link.css('display', 'none');
-//   });
-//   close.click(function(){
-//     menu.removeClass('blockmenu_active');
-//   });
-//   close.click(function(){
-//     link.css('display', 'block');
-//   });
+  link.click(function(){
+    menu.addClass('blockmenu_active');
+    link.css('display', 'none');
+  });
+  close.click(function(){
+    menu.removeClass('blockmenu_active');
+  });
+  close.click(function(){
+    link.css('display', 'block');
+  });
 
-// });
+});
