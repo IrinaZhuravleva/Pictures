4204c5953347135490cd256b91bcacc3ccc6e6ba
Irina Zhuravleva <irinazhuravleva@MacBook-Pro-Irina.local>
2018-12-14 13:05:48 +0300

    Add

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-$(document).ready(function(){
-  var link = $('.navbar-toggler');
-  // var link = $('.main-nav__toggle');
-  var link_active = $('.blockmenu_active');
-  var menu = $('.blockmenu');
-  var close = $('.blockmenu-close');
+// Вызов библиотеки wow.js
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       100,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
 
 
-  link.click(function(){
-    menu.addClass('blockmenu_active');
-    link.css('display', 'none');
-  });
-  close.click(function(){
-    menu.removeClass('blockmenu_active');
-  });
-  close.click(function(){
-    link.css('display', 'block');
-  });
+// https://github.com/kylefox/jquery-modal#installation
 
+$('a.open-modal').click(function(event) {
+  $(this).modal({
+    fadeDuration: 250
+  });
+  return false;
 });
